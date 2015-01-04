542d4e96fa4abed8e078bc86c496ddcfe3bf916c
Xavier Folch <xfolch@gmail.com>
2015-01-04 12:43:16 +0100

    Improve my poor English ;)

--- src/test/scala/me/xfolch/kata/studiousstudent/LexicographicGameTest.scala
+++ src/test/scala/me/xfolch/kata/studiousstudent/LexicographicGameTest.scala
@@ -9,40 +9,42 @@ import org.scalatest.{Matchers, FlatSpec}
  */
 class LexicographicGameTest extends FlatSpec with Matchers {
 
-  "Since an empty input" should "return an empty output" in {
+  "Given an empty input" should "return an empty output" in {
     LexicographicGameFactory().play(EmptyInput) should be (EmptyOutput)
   }
 
-  "Since a text that contains an empty string" should "return an empty output" in {
+  "Given a text that contains an empty string" should "return an empty output" in {
     LexicographicGameFactory().play(Input("")) should be (EmptyOutput)
   }
 
-  "Since more than 100 wordsets" should "throw an exception" in {
+  "Given more than 100 wordsets" should "throw an exception" in {
     val invalidInput = "101\n" + (1 to 101).map(_ => "1 hello").mkString("\n")
     an [IllegalArgumentException] should be thrownBy LexicographicGameFactory().play(Input(invalidInput))
   }
 
-  "Since a text that contains the number '1' in the first line " +
+  "Given a text that contains the number '1' in the first line " +
     "and has no more lines" should "throw an exception" in {
      an [IllegalArgumentException] should be thrownBy LexicographicGameFactory().play(Input("1"))
   }
 
-  "Since a text that contains a number greater than 0 " +
+  "Given a text that contains a number greater than 0 " +
     "in the first line and has no more lines" should "throw an exception" in {
     an [IllegalArgumentException] should be thrownBy LexicographicGameFactory().play(Input("5"))
   }
 
-  "Since a text that contains 1 wordset with 1 word" should "return the word" in {
+  "Given a text that contains 1 wordset with 1 word" should "return that word" in {
     val input =
       """1
         |1 hello""".stripMargin
 
-    val output = "hello"
+    val expectedOutput = "hello"
 
-    LexicographicGameFactory().play(Input(input)).toString should be (output)
+    LexicographicGameFactory().play(Input(input)).toString should be (expectedOutput)
   }
 
-  "Since a text that contains any wordset with a word that has more than 10 letters" should "throw an exception" in {
+  "Given a text that contains any wordset with a word that has more than 10 letters" should
+    "throw an exception" in {
+
     val invalidInput =
       """1
         |1 hellohowareyou""".stripMargin
@@ -50,7 +52,9 @@ class LexicographicGameTest extends FlatSpec with Matchers {
     an [IllegalArgumentException] should be thrownBy LexicographicGameFactory().play(Input(invalidInput))
   }
 
-  "Since a text that contains 1 wordset and it has more elements than expected" should "throw an exception" in {
+  "Given a text that contains 1 wordset and it has more elements than expected" should
+    "throw an exception" in {
+
     val invalidInput =
       """1
         |1 hello john""".stripMargin
@@ -58,7 +62,7 @@ class LexicographicGameTest extends FlatSpec with Matchers {
     an [IllegalArgumentException] should be thrownBy LexicographicGameFactory().play(Input(invalidInput))
   }
 
-  "Since a text that contains 1 wordset and it has more than 9 words" should "throw an exception" in {
+  "Given a text that contains 1 wordset that has more than 9 words" should "throw an exception" in {
     val invalidInput =
       """1
         |10 hello john would you like to go to the cinema""".stripMargin
@@ -66,69 +70,74 @@ class LexicographicGameTest extends FlatSpec with Matchers {
     an [IllegalArgumentException] should be thrownBy LexicographicGameFactory().play(Input(invalidInput))
   }
 
-  "Since a text that contains 1 wordset (2 words ordered)" should "return the following" in {
+  "Given a text that contains 1 wordset (2 words ordered)" should "return the expected output" in {
     val input =
       """1
         |2 hello john""".stripMargin
 
-    val output = "hellojohn"
+    val expectedOutput = "hellojohn"
 
-    LexicographicGameFactory().play(Input(input)).toString should be (output)
+    LexicographicGameFactory().play(Input(input)).toString should be (expectedOutput)
   }
 
-  "Since a text that contains 1 wordset (2 words disordered)" should "return the following" in {
+  "Given a text that contains 1 wordset (2 words disordered)" should "return the expected output" in {
     val input =
       """1
         |2 hello bob""".stripMargin
 
-    val output = "bobhello"
+    val expectedOutput = "bobhello"
 
-    LexicographicGameFactory().play(Input(input)).toString should be (output)
+    LexicographicGameFactory().play(Input(input)).toString should be (expectedOutput)
   }
 
-  "Since a text that contains 2 wordsets (2 words and 1 word respectively)" should "return the following" in {
+  "Given a text that contains 2 wordsets (2 words and 1 word respectively)" should
+    "return the expected output" in {
+
     val input =
       """2
         |2 hello john
         |1 hi""".stripMargin
 
-    val output =
+    val expectedOutput =
       """hellojohn
         |hi""".stripMargin
 
-    LexicographicGameFactory().play(Input(input)).toString should be (output)
+    LexicographicGameFactory().play(Input(input)).toString should be (expectedOutput)
   }
 
-  "Since a text that contains 2 wordsets, each of them contains 2 words" should "return the expected output" in {
+  "Given a text that contains 2 wordsets, each of them contains 2 words" should
+    "return the expected output" in {
+
     val input =
       """2
         |2 hello john
         |2 hi anne""".stripMargin
 
-    val output =
+    val expectedOutput =
       """hellojohn
         |annehi""".stripMargin
 
-    LexicographicGameFactory().play(Input(input)).toString should be (output)
+    LexicographicGameFactory().play(Input(input)).toString should be (expectedOutput)
   }
 
-  "Since a text that contains " +
-    "'3\n2 hello john\n2 hi anne\n3 would you like'" should "return 'hellojohn\nannehi\nlikewouldyou'" in {
+  "Given a text that contains 3 wordsets (two with 2 words and one with 3 words)" should
+    "return the expected output" in {
+
     val input =
       """3
         |2 hello john
         |2 hi anne
         |3 would you like""".stripMargin
 
-    val output =
+    val expectedOutput =
       """hellojohn
         |annehi
         |likewouldyou""".stripMargin
 
-    LexicographicGameFactory().play(Input(input)).toString should be (output)
+    LexicographicGameFactory().play(Input(input)).toString should be (expectedOutput)
   }
 
-  "Since the text proposed in exercise" should "return the expected output" in {
+  "Given the text proposed in exercise" should "return the expected output" in {
     val exerciseInput =
       """5
         |6 facebook hacker cup for studious students
@@ -137,14 +146,14 @@ class LexicographicGameTest extends FlatSpec with Matchers {
         |5 jibw ji jp bw jibw
         |5 uiuy hopji li j dcyi""".stripMargin
 
-    val exerciseOutput =
+    val exerciseExpectedOutput =
       """cupfacebookforhackerstudentsstudious
         |duzklvrawqrc
         |dyroiymybeaxeyubxzdr
         |bwjijibwjibwjp
         |dcyihopjijliuiuy""".stripMargin
 
-    LexicographicGameFactory().play(Input(exerciseInput)).toString should be (exerciseOutput)
+    LexicographicGameFactory().play(Input(exerciseInput)).toString should be (exerciseExpectedOutput)
   }
 
 }
